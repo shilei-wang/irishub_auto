ffcdab28ac7edec81c2cbaea88cd7b63fa138ba4
shelwin_wang <147543282@qq.com>
2019-03-19 11:13:02 +0800

    0非法地址

--- autotest-cmd/autoStart/utils/types.go
+++ autotest-cmd/autoStart/utils/types.go
@@ -8,7 +8,7 @@ import (
 var Common = &CommonWorker{}
 
 // global vars
-var HOME        = "/Users/zjb/"
+var HOME        = "/root/"
 var PASSWORD    = "1234567890"
 var DURATION    = 2
 var BLOCK_TIME  = "5"
